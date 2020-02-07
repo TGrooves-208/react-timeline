9daa7032a3aa6253a821688236806ffa6fa3cbf6
redeyedev-208 <redeyedev.208@gmail.com>
2020-02-07 05:05:38 -0700

    Update build config

---
+++ src/data/Data.js
@@ -0,0 +1,67 @@
+const timelineData = [
+  {
+    text: "LinkedIn",
+    date: "Feb 10 2020",
+    category: {
+      tag: "LinkedIn",
+      color: "#05264C"
+    },
+    link: {
+      url: "https://www.linkedin.com/in/gil-aguilar/",
+      text: "Gil Aguilar | LinkedIn"
+    }
+  },
+
+  {
+    text: "Social Network for Developers",
+    date: "Feb 7 2020",
+    category: {
+      tag: "Heroku",
+      color: "#05264C"
+    },
+    link: {
+      url: "https://secure-woodland-87397.herokuapp.com/login",
+      text: "MongoDb | Express | React | Node"
+    }
+  },
+
+  {
+    text: "Contact List",
+    date: "Feb 6 2020",
+    category: {
+      tag: "Netlify",
+      color: "#05264C"
+    },
+    link: {
+      url: "https://dazzling-noyce-9cdb11.netlify.com/",
+      text: "React | Redux-Thunk | JSON Placeholder"
+    }
+  },
+
+  {
+    text: "Trello Draggable Demo",
+    date: "Feb 5, 2020",
+    category: {
+      tag: "Netlify",
+      color: "#05264C"
+    },
+    link: {
+      url: "https://thirsty-mirzakhani-10e390.netlify.com/",
+      text: "React | DragDropContext"
+    }
+  },
+  {
+    text: "Type & Speak",
+    date: "Feb 01 2020",
+    category: {
+      tag: "GitHub",
+      color: "#05264C"
+    },
+    link: {
+      url: "https://tgrooves-208.github.io/typenspeak_208/",
+      text: "HTML | JS | BS | Web Speech API"
+    }
+  }
+];
+
+export default timelineData;
